48a345b91b114eb775e0fcacc817be31ffa63ce8
João Victor RP <jvrp1000@gmail.com>
2025-05-19 09:51:27 -0300

    Create questao1-struct.c

---
+++ struct/questao1-struct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct{
+    int dia, mes, ano
+}Data;
+
+typedef struct{
+    int hora, minuto, segundo;
+}Horario;
+
+typedef struct{
+
+    char texto[100];
+    Data data;
+    Horario horario
+}Compromisso;
+
+int main(){
+
+    Compromisso c1;
+
+
+}
