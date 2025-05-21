b3a244a269d0f07c0da8c112dc761721c562d976
João Victor RP <jvrp1000@gmail.com>
2025-05-21 09:56:27 -0300

    Update questao5-struct.c

--- struct/questao5-struct.c
+++ struct/questao5-struct.c
@@ -24,5 +24,23 @@ void dadosAluno(Aluno *v, int tam){
         printf("Digite as notas: ");
         scanf("%f %f %f", &v[i].nota[0], &v[i].nota[1], &v[i].nota[2]);
     }
+}
 
+void encontrar(Aluno *v, int *indice){
+    //maior nota da primeira
+    indice[0] = 0;
+    //maior media
+    indice[1] = 0;
+    //menor media
+    indice[2] = 0;
+    for(int i = 1; i < 5; i ++){
+        //verificar maior nota da primeira prova
+        if(v[i].nota[0] > v[indice[0].notas[0])
+            indice[0] = i;
+        //verificar maior media
+        if((v[indice[1].nota[0] + v[indice[1].nota[1] + v[indice[1].nota[2]) / 3 < (v[indice[1].nota[0] + v[indice[1].nota[1] + v[indice[1].nota[2]) / 3)
+            indice[1] = i;
+        if((v[indice[1].nota[0] + v[indice[1].nota[1] + v[indice[1].nota[2]) / 3 < (v[indice[1].nota[0] + v[indice[1].nota[1] + v[indice[1].nota[2]) / 3)
+            indice[2] = i;
+    }
 }
