81ca3c5aba6aa4418f9912009fd80332e9a45644
João Victor RP <jvrp1000@gmail.com>
2025-06-30 09:43:42 -0300

    Create Questao-4.c

---
+++ Recursividade/Lista_de_Exercícios/Questao-4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int fatorial(int n);
+int main()
+{
+    
+    int result = fatorial(5);
+    
+    printf("%d", result);
+    return 0;
+}
+
+int fatorial(int n){
+    if(n == 0){
+        return 1;
+    }else{
+        return n * fatorial(n - 1);
+    }
+}
