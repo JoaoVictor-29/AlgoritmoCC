91837f4ba65512679ce04e8cf9ca5d684cc573c4
João Victor RP <jvrp1000@gmail.com>
2025-07-15 11:26:06 -0300

    Update Questao-2.c

--- Recursividade/Lista_de_Exercícios_2/Questao-2.c
+++ Recursividade/Lista_de_Exercícios_2/Questao-2.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-int fatorial(int n);
-int calcularSerie(int n);
+float fatorial(int n);
+float calcularSerie(int n);
 int main()
 {
     //float vet[] = {3, 2, 1, 4};
     
     fatorial(4);
-    int resp = calcularSerie(4);
-    printf("%d", resp);
+    float resp = calcularSerie(4);
+    printf("%.2f", resp);
     return 0;
 }
 
-int fatorial(int n){
+float fatorial(int n){
     if(n == 0)
         return 1;
     else
-        return n * fatorial(n - 1);
+        return (float)n * fatorial(n - 1);
 }
 
-int calcularSerie(int n){
+float calcularSerie(int n){
     if(n == 1){
         return 1;
     }else{
-        return calcularSerie(n * n) / fatorial(n - 1); 
+        return ((float)(n * n) / fatorial(n)) + calcularSerie(n - 1); 
     }
 }
